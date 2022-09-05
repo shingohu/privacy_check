aea5cd3ef6a3d4b840fd153b73077ae9306975af
shingo <shingo@hellotalk.com>
2022-09-05 17:44:09 +0800

    添加依赖地址

--- android/.cxx/cmake/debug/armeabi-v7a/CMakeFiles/3.10.2/CMakeSystem.cmake
+++ android/.cxx/cmake/debug/armeabi-v7a/CMakeFiles/3.10.2/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "21.3.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/mac_bigsur/Library/Android/sdk/ndk/20.1.5948944/build/cmake/android.toolchain.cmake")
+include("/Users/mac_bigsur/Library/Android/sdk/ndk/21.1.6352462/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
